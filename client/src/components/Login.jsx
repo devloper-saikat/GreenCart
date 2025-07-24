ba4bcc84c3a8b7abbcd09449664b9970d3950fa0
Saikat Bera <devlopersaikat@gmail.com>
2025-07-24 22:47:36 +0530

    complate productCategory components

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -38,7 +38,7 @@ const Login = () => {
             <input
               onChange={(e) => setName(e.target.value)}
               value={name}
-              placeholder="type here"
+              placeholder="Saikat Bera"
               className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
               type="text"
               required
@@ -50,7 +50,7 @@ const Login = () => {
           <input
             onChange={(e) => setEmail(e.target.value)}
             value={email}
-            placeholder="type here"
+            placeholder="saikat@gmail.com"
             className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
             type="email"
             required
@@ -61,7 +61,7 @@ const Login = () => {
           <input
             onChange={(e) => setPassword(e.target.value)}
             value={password}
-            placeholder="type here"
+            placeholder="********"
             className="border border-gray-200 rounded w-full p-2 mt-1 outline-primary"
             type="password"
             required
